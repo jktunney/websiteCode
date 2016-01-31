62bd19a17083e0d02b4ca16562f20b3860c88182
Jake <jktunney5@gmail.com>
2016-01-31 11:16:18 -0500

    yo

---
+++ script.js
@@ -0,0 +1,9 @@
+
+      $(function() {
+        $("[name=toggler]").click(function(){
+            $('.toHide').hide();
+            $("#blk-"+$(this).val()).show('slow');
+        });
+    });
+
+ 
